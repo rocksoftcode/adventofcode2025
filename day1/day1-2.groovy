c96dafe80d3f6b3a0e7bad4ebbcdb0dfd860fcbc
Developer <developer@The-MacBook-Of-Heavy-Metal.local>
2025-12-01 10:28:59 -0600

    Initial commit

---
+++ day1/day1-2.groovy
@@ -0,0 +1,16 @@
+def turns = new File('day1.txt').readLines()
+def cur = 50
+def password = 0
+turns.each {
+    def dir = it.substring(0, 1)
+    def amt = it.substring(1).toInteger()
+    def op = dir == 'L' ? -1 : 1
+    amt.times {
+        if (cur == 0) password++
+        cur += op
+        if (cur < 0) cur = 99
+        else if (cur > 99) cur = 0
+    }
+}
+
+println password
